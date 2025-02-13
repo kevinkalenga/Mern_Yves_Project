c9b8fc5ce2e1bdc804ee03dc93cf35eaf326f2e3
kevin kalenga <kevinkalenga10@gmail.com>
2025-02-13 15:23:29 +0100

    Création de page et des routes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,27 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import About from './pages/About'
 
 function App() {
 
 
   return (
-    <h1 className="bg-red-600 text-center p-6 mb-0 text-white font-bold">Mon super site</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+
+
+    </BrowserRouter>
   )
 }
 
