36c6033119718b25e464b695ae8c8ff55af26865
kevin kalenga <kevinkalenga10@gmail.com>
2025-03-15 21:39:27 +0100

    Création de middleware et d'une fonction permettant à la gestion d'erreur

--- back/controllers/auth.controller.js
+++ back/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs"
+import bcryptjs from "bcryptjs";
+import { errorHandler } from '../utils/errors.js'
 
 // Api signup(inscription)
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
+
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
     const newUser = new User({ username, email, password: hashedPassword });
@@ -12,7 +14,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json("Creation de l'utilisateur reussie")
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
 
 }
